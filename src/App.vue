f9ead921d198961c80f13d41e363bf988f43dbaf
Ivan <119692783+icantillo0422@users.noreply.github.com>
2023-07-19 16:04:51 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="max-w-screen-2xl w-11/12 mx-auto md:w-full flex flex-col gap-y-4 py-5">
+    <h1 class="font-bold text-2xl text-center uppercase">Escanear codigo QR</h1>
+    <section class="flex justify-center">
+      <QrScan @resolve="onResolve" />
+    </section>
+    <section v-show="result" class="flex flex-col justify-center text-center gap-y-5">
+      <p><strong>Codigo:</strong> {{ result }}</p>
+      <button @click="() => {result = ''}" class="w-8/12 md:w-5/12 xl:w-3/12 mx-auto rounded-2xl bg-black text-white py-2">Reiniciar</button>
+    </section>
+  </main>
+</template>
+
+<script setup lang="ts">
+  import QrScan from './components/QrScan.vue';
+  import { ref } from 'vue';
+
+  const result = ref<string>();
+
+  const onResolve = ({ error, data }: { error: boolean, data: string }) => {
+    if (error) {
+      return
+    }
+
+    result.value = data;
+  }
+</script>
---
+++ src/components/QrScan.vue
@@ -0,0 +1,98 @@
+<template>
+  <qrcode-stream 
+    @init="onInit"
+    @detect="onDetect"
+    :paused="paused"
+    :track="paintOutline"
+  />
+</template>
+
+<script setup lang="ts">
+  import { ref } from "vue";
+  import { QrcodeStream } from "vue-qrcode-reader";
+
+  const paused = ref<boolean>(false);
+  const isValid = ref<boolean>();
+  const result = ref<string>();
+
+  const emit = defineEmits(['resolve'])
+
+  const onDetect =  async([ firstDetectedCode ]: [any]) => {
+    result.value = firstDetectedCode.rawValue
+    paused.value = true
+
+    // pretend it's taking really long
+    await timeout(2000)
+    isValid.value = Boolean(result.value);
+
+    emit("resolve", {
+      error: !isValid.value,
+      data: result.value
+    });
+
+    // some more delay, so users have time to read the message
+    paused.value = false
+    result.value = undefined
+    isValid.value = undefined
+  }
+
+  const paintOutline = (detectedCodes: any, ctx: any) => {
+    for (const detectedCode of detectedCodes) {
+      const [firstPoint, ...otherPoints] = detectedCode.cornerPoints
+
+      ctx.strokeStyle = 'red'
+
+      ctx.beginPath()
+      ctx.moveTo(firstPoint.x, firstPoint.y)
+      for (const { x, y } of otherPoints) {
+        ctx.lineTo(x, y)
+      }
+      ctx.lineTo(firstPoint.x, firstPoint.y)
+      ctx.closePath()
+      ctx.stroke()
+    }
+  }
+
+  const timeout = (ms: number) => new Promise((resolve) => {
+    window.setTimeout(resolve, ms)
+  });
+
+  const onInit = async (promise: Promise<any>) => {
+    try {
+      const { capabilities } = await promise;
+      console.log(capabilities)
+    } catch (error: any) {
+      if (error.name === "NotAllowedError") {
+        error = "ERROR: you need to grant camera access permisson";
+      } else if (error.name === "NotFoundError") {
+        error = "ERROR: no camera on this device. Try upload option.";
+      } else if (error.name === "NotSupportedError") {
+        error = "ERROR: secure context required (HTTPS, localhost)";
+      } else if (error.name === "NotReadableError") {
+        error = "ERROR: is the camera already in use?";
+      } else if (error.name === "OverconstrainedError") {
+        error = "ERROR: installed cameras are not suitable";
+      } else if (error.name === "StreamApiNotSupportedError") {
+        error = "ERROR: Stream API is not supported in this browser";
+      }
+    }
+  }
+</script>
+
+<style>
+  .qrcode-stream-wrapper {
+    @apply flex max-w-[450px] w-full h-[270px] md:h-[340px] aspect-[450/340] relative overflow-clip rounded-3xl
+  }
+
+  .qrcode-stream-camera {
+    @apply max-w-[450px] w-full h-[270px] md:h-[340px] aspect-[450/340]
+  }
+
+  .qrcode-stream-camera {
+    @apply absolute top-0 left-0 max-w-[450px] w-full h-[270px] md:h-[340px] aspect-[450/340]
+  }
+
+  .qrcode-stream-overlay {
+    @apply absolute top-0 left-0 max-w-[450px] w-full h-[270px] md:h-[340px] aspect-[450/340]
+  }
+</style>
